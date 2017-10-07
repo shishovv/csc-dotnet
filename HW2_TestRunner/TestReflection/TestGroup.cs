3583391b0c84d314c8a42a7bf1f3ed6d2070fa71
shishov_vlad <vshov@hotmail.com>
2017-10-07 23:44:05 +0300

    hw#2

---
+++ HW2_TestRunner/TestReflection/TestGroup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace TestReflection
+{
+    public class TestGroup
+    {
+        public List<MethodInfo> BeforeClassMethods { get; } = new List<MethodInfo>();
+        public List<MethodInfo> BeforeMethods { get; } = new List<MethodInfo>();
+        public List<MethodInfo> TestMethods { get; } = new List<MethodInfo>();
+        public List<MethodInfo> AfterMethods { get; } = new List<MethodInfo>();
+        public List<MethodInfo> AfterClassMethods { get; } = new List<MethodInfo>();
+            
+        public TestGroup(
+            IEnumerable<MethodInfo> methods, 
+            TestAttributes testAttributes)
+        {
+            foreach (var method in methods)
+            {
+                var attrs = method.GetCustomAttributes().Select(attr => attr.GetType());
+                if (attrs.Contains(testAttributes.BeforeClassAttr))
+                {
+                    BeforeClassMethods.Add(method);
+                }
+                else if (attrs.Contains(testAttributes.BeforeAttr))
+                {
+                    BeforeMethods.Add(method);
+                }
+                else if (attrs.Contains(testAttributes.TestAttr))
+                {
+                    TestMethods.Add(method);
+                }
+                else if (attrs.Contains(testAttributes.AfterAttr))
+                {
+                    AfterMethods.Add(method);
+                }
+                else if (attrs.Contains(testAttributes.AfterClassAttr))
+                {
+                    AfterClassMethods.Add(method);
+                }
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return !TestMethods.Any();
+        }
+    }
+}
---
+++ HW2_TestRunner/TestReflection/TestResult.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TestReflection
+{
+    public class TestResult
+    {
+
+        public Result Res { get; }
+        public TimeSpan Time { get;  }
+        public String IgnoreReason { get; }
+
+        public TestResult(Result res, long time, String ignoreReason)
+        {
+            Res = res;
+            Time = TimeSpan.FromMilliseconds(time);
+            IgnoreReason = ignoreReason;
+        }
+
+        public static TestResult CreateNew(Result res, long time, String IgnoreReason = null)
+        {
+            return new TestResult(res, time, IgnoreReason);
+        }
+
+        public enum Result
+        {
+            Passed,
+            Failed,
+            Skipped
+        }
+    }
+}
